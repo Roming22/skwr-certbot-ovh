83a3c58d23de7f4c133bdb62ef5746730f7f3acb
Roming22 <roming22@gmail.com>
2023-01-17 19:09:36 -0500

    Add message to highlight bug
    There is a permission mismatch. The SSL files are created as root
    but need to be accessible to the traefik user (65532).
    This should be fix in the future, but in the meantime a helpful
    message will be displayed to the user.

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ kubectl apply -k "$SRC_DIR"
 
 # Generate certificate
 kubectl create job "certbot-ovh-manual-$(date +%s)" --namespace kube-system --from cronjob/certbot-ovh
+
+# WARNING
+echo "Run \`sudo chown -R 65532:65532 /mnt/nas/cluster/k3s/storage/persistentvolumes/kube-system/ssl\` on the kubernetes node to fix a permission issue."
